7bf116f6ecba629b924d8670472ee64d812dcf02
Akshay Rai <rai.axay@gmail.com>
2024-08-25 10:31:35 +0530

    added bash aliases

---
+++ .bashrc
@@ -0,0 +1,14 @@
+# .bashrc
+
+if [[ -f ~/bash/bash_aliases.sh ]]; then
+  source bash/bash_aliases.sh
+fi
+
+if [[ -f ~/bash/bash_prompt.sh ]]; then
+  source bash/bash_prompt.sh
+fi
+
+if [[ -f ~/bash/bash_functions.sh ]]; then
+  source bash/bash_functions.sh
+fi
+
---
+++ bash/bash_aliases.sh
@@ -0,0 +1,14 @@
+# bash aliases
+alias lsa='ls -a'
+alias rmf='rm -rf'
+alias g='gvim'
+alias c='clear'
+alias q='exit'
+
+# git specific aliases
+alias gs='git status'
+alias gl='git log'
+alias gb='git branch'
+alias gcm='git commit -m'
+alias gcne='git commit --amend --no-edit'
+
